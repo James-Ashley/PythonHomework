a46a3e22554f9f2dca6ee9685de07df06391f500
James-Ashley <james.kb.ashley@gmail.com>
2020-09-18 11:34:17 -0400

    calculated indices

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,62 +1,13 @@
 # * In this challenge, you are tasked with creating a Python script for analyzing the financial records of your company. You will give a set of financial data called [budget_data.csv](PyBank/Resources/budget_data.csv). The dataset is composed of two columns: `Date` and `Profit/Losses`. (Thankfully, your company has rather lax standards for accounting so the records are simple.)
 
 # * Your task is to create a Python script that analyzes the records to calculate each of the following:
-import os
-import csv
-
-budget_data = os.path.join("Resources" , "budget_data.csv")
-
-with open (budget_data, 'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ',')
-    next(csvreader)
-
-
-#   * The total number of months included in the dataset
-    totalMonths = []
-    for row in csvreader:
-        totalMonths.append(int(row[1]))
-        months = len(totalMonths)
-        profitsTotal = sum(totalMonths)
-
-with open (budget_data, 'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ',')
-    next(csvreader)
-
-    delta = []
-    prevMonth = 0
-    for row in csvreader:
-        if prevMonth == 0:
-            prevMonth = int(row[1])
-        else:
-            delta.append(int(row[1])-prevMonth)
-            prevMonth = int(row[1])
-    avgDelta = sum(delta) / len(delta)
-
-greatestIncrease = max(delta)
-greatestDecrease = min(delta)
-
-increaseIndex = delta.index(greatestIncrease)+1
-decreaseIndex = delta.index(greatestDecrease)+1
-
-with open (budget_data, 'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ',')
-    next(csvreader)
-
-
-
-   
-
-
-            
-        
-
 
 #   * The net total amount of "Profit/Losses" over the entire period      
 #   * The average of the changes in "Profit/Losses" over the entire period
 #   * The greatest increase in profits (date and amount) over the entire period
 #   * The greatest decrease in losses (date and amount) over the entire period
 # * As an example, your analysis should look similar to the one below:
-
+#   * The total number of months included in the dataset
 #   ```text
 #   Financial Analysis
 #   ----------------------------
@@ -68,6 +19,56 @@ with open (budget_data, 'r') as csvfile:
 #   ```
 
 # * In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+import os
+import csv
+
+budget_data = os.path.join("Resources" , "budget_data.csv")
+
+with open (budget_data, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    header = next(csvreader)
+    
+    delta = []
+    prevMonth = 0
+    totalMonths = []
+    monthsList = []
+    greatestIncrease = 0
+    greatestDecrease = 0
+    increaseIndex = 0
+    decreaseIndex = 0
+
+    for row in csvreader:
+        totalMonths.append(int(row[1]))
+        monthsList.append(row[0])
+        months = len(totalMonths)
+        profitsTotal = sum(totalMonths)
+        
+        if prevMonth == 0:
+            prevMonth = int(row[1])
+        else:
+            delta.append(int(row[1])-prevMonth)
+            prevMonth = int(row[1])
+
+avgDelta = sum(delta) / len(delta)
+
+greatestIncrease = max(delta)
+greatestDecrease = min(delta)
+
+increaseIndex = delta.index(greatestIncrease)
+decreaseIndex = delta.index(greatestDecrease)
+
+print(greatestIncrease , greatestDecrease , monthsList[increaseIndex+1] , monthsList[decreaseIndex+1], avgDelta, months, profitsTotal)
+
+
+
+
+   
+
+
+            
+        
+
+
 
 
 
